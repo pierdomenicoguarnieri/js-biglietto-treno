c7568e9a297b963ba979f3fe00954995b2233a15
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-20 22:09:15 +0100

    add: insert price in offcanvas using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ let price = (km * 0.21);
 
 const output = document.getElementById("content");
 
+const outputOffcanvas = document.getElementById("outputOffcanvas");
+
 if(age < 18){
   const originalPrice = price;
   const percentage = (price / 100);
@@ -13,6 +15,10 @@ if(age < 18){
   output.innerHTML = `
   <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
   <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 20% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
+  `
+
+  outputOffcanvas.innerHTML = `
+  <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
   `
 }else if(age >= 65){
   const originalPrice = price;
@@ -22,9 +28,17 @@ if(age < 18){
   <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
   <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 40% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
   `
+
+  outputOffcanvas.innerHTML = `
+  <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
+  `
 }else{
   output.innerHTML = `
   <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
   <p class="fs-5 pb-3">Il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>. Non è stato applicato nessuno sconto.</p>
+  `
+
+  outputOffcanvas.innerHTML = `
+  <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
   `
 }

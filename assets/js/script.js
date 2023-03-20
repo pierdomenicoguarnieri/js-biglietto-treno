bd18a29759fb3582cf769b197c43ccf1deeab05d
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-20 19:37:06 +0100

    changed: output message

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,20 @@ if(age < 18){
   const percentage = (price / 100);
   price = price - (percentage * 20);
   output.innerHTML = `
-  <h1>Ciao! Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</h1> 
-  <h2>Il prezzo del biglietto è: ${price.toFixed(2)}€</h2>
+  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
+  <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 20% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
   `
 }else if(age >= 65){
   const originalPrice = price;
   const percentage = (price / 100);
   price = price - (percentage * 40);
   output.innerHTML = `
-  <h1>Ciao! Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</h1> 
-  <h2>Il prezzo del biglietto è: ${price.toFixed(2)}€</h2>
+  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
+  <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 40% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
   `
 }else{
   output.innerHTML = `
-  <h1>Ciao! Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</h1> 
-  <h2>Il prezzo del biglietto è: ${price.toFixed(2)}€</h2>
+  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
+  <p class="fs-5 pb-3">Il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>. Non è stato applicato nessuno sconto.</p>
   `
 }

d6c06e266cdd95b672a47e6d13bb4f3688da15ae
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-21 10:26:38 +0100

    fix: typos in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ if(age < 18){
   const originalPrice = price;
   price *= 1 - (20/100);
   output.innerHTML = `
-  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
+  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai inserito la seguente età: ${age} anni.</span> 
   <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 20% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
   `
 
@@ -23,7 +23,7 @@ if(age < 18){
   const originalPrice = price;
   price *= 1 - (40/100);
   output.innerHTML = `
-  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
+  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai inserito la seguente età: ${age} anni.</span> 
   <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 40% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
   `
 
@@ -32,7 +32,7 @@ if(age < 18){
   `
 }else{
   output.innerHTML = `
-  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai iserito la seguente età: ${age} anni.</span> 
+  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai inserito la seguente età: ${age} anni.</span> 
   <p class="fs-5 pb-3">Il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>. Non è stato applicato nessuno sconto.</p>
   `
 
